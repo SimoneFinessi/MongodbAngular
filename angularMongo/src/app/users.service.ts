bcda26a88d6810e087c404722ac2ea86f3d976d3
SimoneFinessi <finessi.simone@iisgalvanimi.edu.it>
2024-10-30 14:55:44 +0000

    finito registrazione

--- angularMongo/src/app/users.service.ts
+++ angularMongo/src/app/users.service.ts
@@ -6,14 +6,14 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class UsersService {
-  private apiUrl = 'http://127.0.0.1:5000/api/utenti';
+  private apiUrl = 'http://127.0.0.1:5000/api/reg';
 
   constructor(private http: HttpClient) { }
 
   // Funzione per aggiungere un nuovo utente
   addUser(username: string, password: string, email: string): Observable<any> {
     const userData = { username, password, email };
-
+    console.log(userData);
     // Invia la richiesta POST con i dati dell'utente
     return this.http.post(this.apiUrl, userData, {
       headers: new HttpHeaders({
