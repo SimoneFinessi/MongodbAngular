c7faa824e9a71cb2193c38238a9a4ab53ebe90c3
SimoneFinessi <finessi.simone@iisgalvanimi.edu.it>
2024-10-30 14:37:31 +0000

    sign up

--- angularMongo/src/app/login/login.component.ts
+++ angularMongo/src/app/login/login.component.ts
@@ -1,12 +1,29 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
-
+import { UsersService } from '../users.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  
+  username: string = '';
+  password: string = '';
+  email: string = '';
+
+  constructor(private userService: UsersService) {}
+
+  // Funzione per inviare i dati dell'utente
+  registerUser() {
+    this.userService.addUser(this.username, this.password, this.email)
+      .subscribe(
+        response => {
+          console.log('User registered successfully:', response);
+        },
+        error => {
+          console.error('Error registering user:', error);
+        }
+      );
+  }
 }
